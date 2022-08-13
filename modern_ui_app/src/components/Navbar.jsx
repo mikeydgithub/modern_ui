54c8e175060a781789cc77c11b6354a23b728bb9
mikeydgithub <ampgzo2@gmail.com>
2022-08-13 16:55:32 -0400

    creating mobile navbar

--- modern_ui_app/src/components/Navbar.jsx
+++ modern_ui_app/src/components/Navbar.jsx
@@ -4,17 +4,26 @@ import { close, logo, menu } from '../assets';
 import { navLinks } from '../constants';
 
 const Navbar = () => {
+
+  const [toggle, setToggle] = useState(false);
+
   return (
-    <nav className='w-full flex py-6 justify-between items-center navbar'>
-      <img src={logo} alt="NewBank" className='w-[124px] h-[32px]'/>
-      <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
+    <nav className="w-full flex py-6 justify-between items-center navbar">
+      <img src={logo} alt="NewBank" className="w-[124px] h-[32px]"/>
+      <ul className="list-none sm:flex hidden justify-end items-center flex-1">
       {navLinks.map((nav, index) => (
         <li
-        key={id}
-        className={`font-poppins
+        key={nav.id}
+        className={  
+        `
+        font-poppins
         font-normal
         cursor-pointer
-        `}
+        text-[16px]
+        text-white
+        ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'}
+        `
+        }
         >
           <a href={`#${nav.id}`}>
             {nav.title}
@@ -22,6 +31,33 @@ const Navbar = () => {
         </li>
       ))}
       </ul>
+      <div 
+      // mobile friendly menu using state to transform the button from menu to X
+        className='sm:hidden flex flex-1 justify-end items-center'>
+        <img
+        src={toggle ? close : menu}
+        alt="menu"
+        className='w-[28px] h-[28px] object-contain'
+        onClick={() => setToggle((prev) => !prev)}
+        />
+      </div>
+      <div
+      // menu toggle to have animated button appear in menu bar
+      className={`${toggle ? 'flex' : 'hidden'} 
+      p-6
+      bg-black-gradient
+      absolute
+      top-20
+      right-0
+      mx-4
+      my-2
+      min-w-[140px]
+      rounded-xl
+      sidebar
+      `
+      }>
+        
+      </div>
     </nav>
   )
 }
