3795c20cb8cc4719da955befd407a5c07089363c
mikeydgithub <ampgzo2@gmail.com>
2022-08-12 20:56:00 -0400

    start developing style for navbar

--- modern_ui_app/src/components/Navbar.jsx
+++ modern_ui_app/src/components/Navbar.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import {useState} from 'react'
+
+import { close, logo, menu } from '../assets';
+import { navLinks } from '../constants';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='w-full flex py-6 justify-between items-center navbar'>
+      <img src={logo} alt="NewBank" className='w-[124px] h-[32px]'/>
+      <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
+      {navLinks.map((nav, index) => (
+        <li
+        key={id}
+        className={`font-poppins
+        font-normal
+        cursor-pointer
+        `}
+        >
+          <a href={`#${nav.id}`}>
+            {nav.title}
+          </a>
+        </li>
+      ))}
+      </ul>
+    </nav>
   )
 }
 
