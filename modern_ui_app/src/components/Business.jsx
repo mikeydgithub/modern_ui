566a8c7474b44b49bc635cc301d65b1d0025b8e6
mikeydgithub <ampgzo2@gmail.com>
2022-08-28 10:20:49 -0400

    feature card icons, content, tilte, and index destructured to put nto feature card component in businessjsx

--- modern_ui_app/src/components/Business.jsx
+++ modern_ui_app/src/components/Business.jsx
@@ -2,9 +2,19 @@ import { features } from '../constants';
 import styles, { layout } from '../style'
 import Button from './Button';
 
-const FeatureCard = () => (
-  <div>
-
+const FeatureCard = ({ icon, title, content, index }) => (
+  <div className={`flex flex-row p-6 rounded-[20px] ${index !== features.length -1 ? "mb-6" : "mb-0"} feature-card`}>
+    <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}>
+      <img src={icon} alt="icon" className='w-[50%] h-[50%] object-contain'/>
+    </div>
+    <div className='flex-1 flex flex-col ml-3'>
+    <h4 className='font-poppins font-semibold text-white text-[18px] leading-[23px] mb-1'>
+      {title}
+    </h4>
+      <p className='font-poppins font-normal text-dimWhite text-[16px] leading-[24px] mb-1'>
+        {content}
+      </p>
+    </div>
   </div>
 )
 
@@ -19,7 +29,7 @@ const Business = () => {
       
       <div className={`${layout.sectionImg} flex-col`}>
         {features.map(( feature, index ) => (
-          <FeatureCard key={feature.id}{...feature}/>
+          <FeatureCard key={feature.id}{...feature} index={index}/>
         ))}
       </div>
     </section>
