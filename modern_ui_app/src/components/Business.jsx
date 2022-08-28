aac8dff99b03bf9462e06c5860786e2e76e07b62
mikeydgithub <ampgzo2@gmail.com>
2022-08-28 09:58:07 -0400

    sunday commit

--- modern_ui_app/src/components/Business.jsx
+++ modern_ui_app/src/components/Business.jsx
@@ -2,6 +2,12 @@ import { features } from '../constants';
 import styles, { layout } from '../style'
 import Button from './Button';
 
+const FeatureCard = () => (
+  <div>
+
+  </div>
+)
+
 const Business = () => {
   return (
     <section id="features" className={layout.section}>
@@ -9,6 +15,12 @@ const Business = () => {
         <h2 className={styles.heading2}>You do the business,<br className='sm:block hidden'/> we'll handle the money.</h2>
         <p className={`${styles.paragraph} max-w-[470px] mt-5`}>With the right credit card, you can imrpove your financial life by building credit, earning rewards, and saving money. But with hundreds of credit cards on the market.</p>
         <Button styles="mt-10"/>
+      </div>
+      
+      <div className={`${layout.sectionImg} flex-col`}>
+        {features.map(( feature, index ) => (
+          <FeatureCard key={feature.id}{...feature}/>
+        ))}
       </div>
     </section>
   )
